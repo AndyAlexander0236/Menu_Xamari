b8bdc76a5fcf2779c84627e9731c812ba2ccddf2
Andy Alexander Logacho <logachologacho@gmail.com>
2024-06-05 22:19:54 -0500

    Agregar archivos de proyecto.

---
+++ Menu/Menu/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Menu.ViewModels;
+using Menu.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Menu
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ Menu/Menu/Views/Notas.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Menu.Models;
+using Menu.ViewModels;
+using Menu.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Menu.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
